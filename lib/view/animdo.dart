35e890bf29f66ce44a43f126a229165abce811d4
summydeveloper <adegbiteadekola54@gmail.com>
2024-10-04 15:44:43 +0100

    done with basic animation

--- lib/view/animdo.dart
+++ lib/view/animdo.dart
@@ -1,10 +1,61 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class AnimDo extends StatelessWidget {
-  const AnimDo({super.key});
+class Animdo extends StatefulWidget {
+  const Animdo({super.key});
 
   @override
+  State<Animdo> createState() => _AnimdoState();
+}
+
+class _AnimdoState extends State<Animdo> {
+  double _buttonRadius = 100.0;
+  // final Tween<double> _backgroundScale= ;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            _pageBackground(),
+            _circularAnimationButton()
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _pageBackground(){
+    return Placeholder(
+      child: Container(
+        color: Colors.blue,
+      ),
+    );
+
+  }
+
+  Widget _circularAnimationButton(){
+    return Center(
+      child: GestureDetector(
+        onTap: (){
+          setState(() {
+            _buttonRadius += _buttonRadius== 200 ? -100 : 100;
+          });
+        } ,
+        child: AnimatedContainer(
+          child: Center( child: Text('Tap, Tap'),),
+
+          width: _buttonRadius,
+          height: _buttonRadius,
+          decoration:   BoxDecoration( color: Colors.purple,
+              borderRadius: BorderRadius.circular(_buttonRadius)), duration: const Duration(seconds: 2),
+
+        ),
+      ),
+    );
+
   }
 }
